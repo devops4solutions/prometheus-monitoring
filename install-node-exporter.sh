4fb29006fe8cfe0ee65036fa162ea1f97f028296
DevOps4Solutions <39961002+devops4solutions@users.noreply.github.com>
2020-12-01 16:46:59 -0500

    Update install-node-exporter.sh

--- install-node-exporter.sh
+++ install-node-exporter.sh
@@ -2,3 +2,10 @@ wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_e
 tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
 sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
+
+sudo cp node-exporter.service /etc/systemd/system/node-exporter.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable node-exporter
+sudo systemctl start node-exporter
+sudo systemctl status node-exporter
