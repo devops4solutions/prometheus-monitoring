ab9fb2043dd946a92b7f3e6b981f4813f114ed86
DevOps4Solutions <39961002+devops4solutions@users.noreply.github.com>
2020-12-01 16:13:15 -0500

    Create install-grafana.sh

---
+++ install-grafana.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y adduser libfontconfig1
+wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
+sudo dpkg -i grafana_7.3.4_amd64.deb
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+sudo systemctl enable grafana-server.service
