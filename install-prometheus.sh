4ba44bd6d65c4638cb9bcf1908b2174fa4fd693f
DevOps4Solutions <39961002+devops4solutions@users.noreply.github.com>
2020-12-01 15:51:18 -0500

    Update install-prometheus.sh

--- install-prometheus.sh
+++ install-prometheus.sh
@@ -11,3 +11,19 @@ sudo cp -r prometheus-2.23.0.linux-amd64/console_libraries /etc/prometheus
 sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin/
 
 rm -rf prometheus-2.23.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
+sudo cp prometheus.yml /etc/prometheus/
+sudo cp prometheus.service /etc/systemd/system/prometheus.service
+
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /var/lib/prometheus
+
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+sudo systemctl start prometheus
+
+
