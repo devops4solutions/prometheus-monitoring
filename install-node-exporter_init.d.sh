f68717bc1b781947b85e7ffe8627f98b6146b4f8
DevOps4Solutions <39961002+devops4solutions@users.noreply.github.com>
2020-12-03 17:13:45 -0500

    Update and rename node-exporter.sh to install-node-exporter_init.d.sh

--- install-node-exporter_init.d.sh
+++ install-node-exporter_init.d.sh
@@ -4,3 +4,8 @@ tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
 sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /opt/node-exporter
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 sudo cp node_exporter.sh /opt/node-exporter
+sudo cp node-exporter-init.d.service /etc/init.d/node_exporter
+sudo chmod +x /etc/init.d/node_exporter
+sudo chmod +x /opt/node-exporter/node_exporter.sh
+chkconfig --add node_exporter
+sudo service node_exporter start
